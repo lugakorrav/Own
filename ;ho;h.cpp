3890b3ffd29bf4d39831de342db8aa0ea2ae1e17
lugakorrav <lugakorrav@yandex.ru>
2014-10-31 15:20:50 +0400

    Create ;ho;h.cpp

---
+++ ;ho;h.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int A=0,B=0,S=0,alen,blen,slen=1;
+	char* a = new char[];
+	char* b = new char[];
+	char* s = new char[];
+	char* t = new char[];
+	cin.getline(a, 32, '\n');
+	cin.getline(b, 32, '\n');
+	for (int i=0; a[i]!=0; i++)
+	{
+		alen=i+1;
+	}
+	for (int i=0; b[i]!=0; i++)
+		blen=i+1;
+	for (int i=0; a[i]!=0; i++)
+	{
+		S=S+((int)(a[i])-(int)'0')*pow(10,alen-i-1)+((int)(b[i])-(int)'0')*pow(10,blen-i-1);
+	}
+	for (int i=0; S>0; i++)
+	{
+		s[i]=(char)((int)'0'+(S%10));
+		S/=10;
+		slen++;
+	}
+	s[slen-1]=0;
+	for (int i=0; i<=slen-1; i++)
+		t[i]=s[slen-i-1];
+	cout<<t;
+	system("pause");
+	return 0;
+}
