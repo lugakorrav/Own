165e8111f4abd6465021e233862c2b0169a01197
lugakorrav <lugakorrav@yandex.ru>
2014-12-23 04:09:39 +0400

    Create turn_class.h

---
+++ Zadanie_5_-2/turn_class.h
@@ -0,0 +1,24 @@
+#include "stdafx.h"
+
+using namespace std;
+
+class Turn	{
+public:
+	Turn();
+	~Turn();
+	void Enqueue(string);
+	std::string Begin();
+	int Length();
+
+	friend std::ostream& operator<<(std::ostream&, const Turn&);
+	friend std::istream& operator>>(std::istream&, Turn&);
+
+private:
+
+	struct TurnElement	{
+		string content;
+		TurnElement* prev = NULL;
+		TurnElement* next = NULL;
+	}	*HeadElement, 
+		*OldElement;
+};
