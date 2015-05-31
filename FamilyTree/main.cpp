0ca2b452efdce27b50fdf7365013e6b2daacd8cb
lugakorrav <lugakorrav@yandex.ru>
2015-06-01 01:29:32 +0400

    Update main.cpp

--- FamilyTree/main.cpp
+++ FamilyTree/main.cpp
@@ -1,14 +1,11 @@
 #include <iostream>
 #include "tree.h"
 
+using namespace std;
+
 int main()
 {
 	setlocale(LC_ALL, "rus");
 	Tree t;
-	t.addhuman("x");
-	t.addhuman("y");
-	t.addlink("y", "x");
-	system("pause");
-	t.findinfo("y");
-	system("pause");
+	t.dialog();
 };
