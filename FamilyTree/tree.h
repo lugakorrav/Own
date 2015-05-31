c5ed1d8ba348abc7e97c6281edd1c4d537290d95
lugakorrav <lugakorrav@yandex.ru>
2015-05-31 22:02:29 +0400

    Update tree.h

--- FamilyTree/tree.h
+++ FamilyTree/tree.h
@@ -14,13 +14,21 @@ private:
 		Human* father;
 		Human* mother;
 		std::map<std::string, Human*> children;
+		bool sex;
 		std::string name;
 
 	public:
-		Human(std::string Aname);
+		Human(std::string Aname, bool Asex);
+		~Human();
+
 		void addparent(Human* Aparent);
 		void addchild(Human* Achild);
 		void addpartner(Human* Apartner);
+
+		void delparent(Human* Aparent);
+		void delchild(Human* Achild);
+		void delpartner(Human* Apartner);
+
 		std::string getname();
 		//Human* checkchildren(Human* h);
 		void show();
@@ -31,11 +39,11 @@ private:
 	Human* find(std::string Aname);
 	void showinfo(Human* h);
 public:
-	void addhuman(const std::string Aname);
+	void addhuman(const std::string Aname, bool Asex);
 	void addlink(std::string ParentName, std::string ChildName);
 	void addpartner(std::string p1, std::string p2);
-	void delhuman(std::string h);
-	void dellink(std::string h);
-	void delpartner(std::string h);
-	void findinfo(std::string Aname);
+	void delhuman(std::string);
+	void dellink(std::string ParentName, std::string ChildName);
+	void delpartner(std::string p1, std::string p2);
+	void findinfo(std::string);
 };
